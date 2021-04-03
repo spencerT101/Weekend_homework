9e8df1f926a34ad0d486d1d96e6cc2fc4f617246
Spencer <Spencer.Taber@yahoo.com>
2021-04-03 19:51:11 +0100

    task 7 completed

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -20,7 +20,7 @@ def get_total_cash(pet_shop):
 def add_or_remove_cash(pet_shop, money):
     pet_shop["admin"]["total_cash"] += money
 
-# task 5 check return number of sold pets
+# task 5  return number of sold pets
 
 def get_pets_sold(pet_shop):
     return pet_shop["admin"]["pets_sold"]
@@ -31,4 +31,36 @@ def  increase_pets_sold(pet_shop, add_number ):
      pet_shop["admin"]["pets_sold"] = add_number
 
 
+# count (LEN) the number of pets for a stock count
+def get_stock_count(pet_shop):
+
+    return len(pet_shop["pets"])
+    
+
+# task 8 return the number of a specific breed
+
+def get_pets_by_breed(pet_shop, breed):
+    
+    if breed in pet_shop == breed:
+        return len(petshop["pets"][0]["breed"] and petshop["pets"][1]["breed"])
+
+
+   
+    
+#     breed_list = []
+    
+#     for breeds in pet_shop:
+#         if breeds == breed:
+#             breed_list.append(breed)
+        
+#         return "found"
+
+# print(get_pets_by_breed(pet_shop, "British Shorthair"))
+
+
+
+
+
+
+    
 
