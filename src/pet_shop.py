4dc84a9e451bacb653f971e7e43133ea5a069275
Spencer <Spencer.Taber@yahoo.com>
2021-04-05 09:12:49 +0100

    updated task numbers

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -15,30 +15,30 @@ def get_total_cash(pet_shop):
     return pet_shop["admin"]["total_cash"]
 
 
-# add or remove cash tasks 3 & 4
+# add or remove cash tasks 4 & 5
 
 def add_or_remove_cash(pet_shop, money):
     pet_shop["admin"]["total_cash"] += money
 
-# task 5  return number of sold pets
+# task 6  return number of sold pets
 
 def get_pets_sold(pet_shop):
     return pet_shop["admin"]["pets_sold"]
 
-# task 6 increase the number of sold pets
+# task 7 increase the number of sold pets
 
 def  increase_pets_sold(pet_shop, add_number ):
      pet_shop["admin"]["pets_sold"] = add_number
 
 
-# count (LEN) the number of pets for a stock count
+# task 8 count (LEN) the number of pets for a stock count
 def get_stock_count(pet_shop):
     
     return len(pet_shop["pets"])
     
 
-# task 8 return the number of a specific breed.
-# task 9 return none if breed doesn't exits
+# task 9 return the number of a specific breed.
+# task 10 return none if breed doesn't exits
 
 def get_pets_by_breed(pet_shop, breed):
 
@@ -52,7 +52,7 @@ def get_pets_by_breed(pet_shop, breed):
 
 
 
- # task 10 return pet by name.
+ # task 11 return pet by name.
 
 def find_pet_by_name(pet_shop, pet_name):
     
@@ -60,7 +60,7 @@ def find_pet_by_name(pet_shop, pet_name):
          if pet["name"] == pet_name:
              return pet
 
-# task 11 remove pet by name.
+# task 12 remove pet by name.
 
 def remove_pet_by_name(pet_shop, pet_name): 
     
@@ -68,21 +68,21 @@ def remove_pet_by_name(pet_shop, pet_name):
          if pet["name"] == pet_name:
              pet_shop["pets"].remove(pet)
 
-#task 12 add pet by name
+#task 13 add pet by name
 
 def add_pet_to_stock(pet_shop, pet_to_add):
     
     pet_shop["pets"].append(pet_to_add)
 
 
-#task 13 get customer cash
+#task 14 get customer cash
 
 
 def get_customer_cash(customer_pets):
 
     return customer_pets["cash"]
 
-# task 14 remove customer cash:
+# task 15 remove customer cash:
 
 def remove_customer_cash(customers, cash_remove):
 
@@ -90,13 +90,13 @@ def remove_customer_cash(customers, cash_remove):
 
     return customers["cash"]
 
-# task get count of pets for a customer
+# task 16 get count of pets for a customer
 
 def get_customer_pet_count (customer_pets):
 
     return len(customer_pets["pets"])
 
-#task 16 add a pet to a customer.
+#task 17 add a pet to a customer.
 
 def add_pet_to_customer(customer_pets, add_pet):
 
